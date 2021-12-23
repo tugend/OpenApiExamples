2967a89cfe7890065010bc58d7411ae57e0f9564
tugend <tugend@gmail.com>
2021-12-23 22:21:53 +0100

    checkpoint

---
+++ ExampledApi/Swagger/Examples/CreatePayment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace WebApi.Infrastructure.Swagger.Examples
+{
+    public class CreatePayment : IExamplesProvider<Controllers.CreatePaymentRequestV1>
+    {
+        public Controllers.CreatePaymentRequestV1 GetExamples()
+        {
+            return new(200);
+        }
+        
+        // public Controllers.CreatePaymentRequestV1 GetExamples()
+        // {
+        //     return new(200, Guid.NewGuid().ToString(), "everyday purchase");
+        // }
+    }
+}
---
+++ ExampledApi/Swagger/Examples/UpdatePayment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace WebApi.Infrastructure.Swagger.Examples
+{
+    public class UpdatePayment : IMultipleExamplesProvider<Controllers.UpdatePayment>
+    {
+        public IEnumerable<SwaggerExample<Controllers.UpdatePayment>> GetExamples()
+        {
+            return new List<SwaggerExample<Controllers.UpdatePayment>>
+            {
+                SwaggerExample.Create("First Example",
+                    new Controllers.UpdatePayment(100, "A payment for new socks", "everyday purchase")),
+                
+                SwaggerExample.Create("Second Example",
+                    new Controllers.UpdatePayment(900, "A payment for new shoes", "everyday purchase"))
+            };
+        }
+    }
+}
