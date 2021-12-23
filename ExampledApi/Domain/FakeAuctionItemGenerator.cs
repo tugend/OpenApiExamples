b735df32085d0d355382de865cab9cab272e8d8b
tugend <tugend@gmail.com>
2021-12-23 23:45:33 +0100

    checkpoint

--- ExampledApi/Domain/FakeAuctionItemGenerator.cs
+++ ExampledApi/Domain/FakeAuctionItemGenerator.cs
@@ -1,19 +1,19 @@
-﻿// using System;
-// using ExampledApi.Controllers.Auction;
-// using ExampledApi.Controllers.Auction.Common;
-// using ExampledApi.Controllers.Auction.GetAuctionedItems;
-// using static ExampledApi.Domain.FakeHelper;
-//
-// namespace ExampledApi.Domain
-// {
-//     public static class FakeAuctionItemGenerator
-//     {
-//         public static Response FakeAuctionedFoodStockResponse() =>
-//             new(
-//                 Guid.NewGuid(),
-//                 100,
-//                 Pick(100, 200, 300, 400, 500, 600, 700, 800, 900),
-//                 Pick(Enum.GetValues<RiceQuality>())
-//             );
-//     }
-// }
+﻿using System;
+using ExampledApi.Controllers.Auction;
+using ExampledApi.Controllers.Auction.Common;
+using ExampledApi.Controllers.Auction.GetAuctionedItems;
+using static ExampledApi.Domain.FakeHelper;
+
+namespace ExampledApi.Domain
+{
+    public static class FakeAuctionItemGenerator
+    {
+        public static Response FakeAuctionedFoodStockResponse() =>
+            new(
+                Guid.NewGuid(),
+                100,
+                Pick(100, 200, 300, 400, 500, 600, 700, 800, 900),
+                Pick(Enum.GetValues<RiceQuality>())
+            );
+    }
+}
